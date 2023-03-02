5074115fd8066b58b853b8abcd279f375b82c2b7
KDW999 <hide4321@naver.com>
2023-03-02 17:50:19 +0900

    board dto

--- board/src/main/java/com/koreait/board/BoardApplication.java
+++ board/src/main/java/com/koreait/board/BoardApplication.java
@@ -3,7 +3,7 @@ package com.koreait.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //! 여기서 실행
 public class BoardApplication {
 
 	public static void main(String[] args) {
